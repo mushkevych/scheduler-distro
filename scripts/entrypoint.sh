1a6041416cb6bd7f69b9506b037a330997251d6d
Bohdan Mushkevych <mushkevych@gmail.com>
2020-03-04 15:31:38 -0800

    Adding --no-clobber to prevent settings_xxx overwrite

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,11 +10,11 @@ until wget http://${RABBITMQ_HOST}:15672/cli/rabbitmqadmin -O /tmp/rabbitmqadmin
 done
 
 # step 2: /opt/synergy-distro/configuration is mapped from the HOST with docker --volume mechanism
-# copy settings_dev.py
+# --no-clobber prevents overwriting of existing files
 touch /opt/synergy-distro/configuration/__init__.py
-mv /opt/synergy-distro/settings_*.py /opt/synergy-distro/configuration/ || true
-mv /opt/synergy-distro/flows_*.py /opt/synergy-distro/configuration/ || true
-mv /opt/synergy-distro/context_*.py /opt/synergy-distro/configuration/ || true
+mv --no-clobber /opt/synergy-distro/settings_*.py /opt/synergy-distro/configuration/ || true
+mv --no-clobber /opt/synergy-distro/flows_*.py /opt/synergy-distro/configuration/ || true
+mv --no-clobber /opt/synergy-distro/context_*.py /opt/synergy-distro/configuration/ || true
 
 # step 3: define a boxid and start the process
 SYN_BOX_ID=${1:-scheduler}
